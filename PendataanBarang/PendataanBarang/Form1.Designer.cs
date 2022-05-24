7bb1cfc55375fd2452df81e82c9e6d244153f532
fahmifahqur <72751538+fahmifahqur@users.noreply.github.com>
2022-05-24 08:12:39 +0700

    Add files via upload

---
+++ PendataanBarang/PendataanBarang/Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace PendataanBarang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.InputButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.DateTimePicker();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.PrintButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kode Barang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Barang";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(89, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jumlah Barang";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(89, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 19);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Jenis Barang";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(89, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Harga Barang";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(89, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 19);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Satuan Barang";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 337);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 19);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Tanggal";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(224, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(224, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(261, 26);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(224, 177);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(261, 26);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(224, 217);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(261, 26);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(224, 257);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(261, 26);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(224, 297);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(261, 26);
+            this.textBox6.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(93, 369);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(862, 348);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // InputButton
+            // 
+            this.InputButton.Location = new System.Drawing.Point(511, 100);
+            this.InputButton.Name = "InputButton";
+            this.InputButton.Size = new System.Drawing.Size(105, 41);
+            this.InputButton.TabIndex = 14;
+            this.InputButton.Text = "Tambah Data";
+            this.InputButton.UseVisualStyleBackColor = true;
+            this.InputButton.Click += new System.EventHandler(this.InputButton_Click);
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(511, 150);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(105, 41);
+            this.UpdateButton.TabIndex = 15;
+            this.UpdateButton.Text = "Ubah Data";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(224, 337);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(261, 26);
+            this.textBox7.TabIndex = 16;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(622, 100);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(105, 41);
+            this.DeleteButton.TabIndex = 17;
+            this.DeleteButton.Text = "Hapus Data";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(622, 150);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(105, 41);
+            this.RefreshButton.TabIndex = 18;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Location = new System.Drawing.Point(511, 322);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 41);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cari Data Barang";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(6, 15);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(372, 26);
+            this.textBox8.TabIndex = 20;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // PrintButton
+            // 
+            this.PrintButton.Location = new System.Drawing.Point(511, 217);
+            this.PrintButton.Name = "PrintButton";
+            this.PrintButton.Size = new System.Drawing.Size(216, 66);
+            this.PrintButton.TabIndex = 20;
+            this.PrintButton.Text = "Simpan Data";
+            this.PrintButton.UseVisualStyleBackColor = true;
+            this.PrintButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1264, 729);
+            this.Controls.Add(this.PrintButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.InputButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aplikasi Pendataan Barang";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button InputButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.DateTimePicker textBox7;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button PrintButton;
+
+    }
+}
+
---
+++ PendataanBarang/PendataanBarang/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using DGVPrinterHelper;
+
+namespace PendataanBarang
+{
+    public partial class Form1 : Form
+    {
+        // Rizki Januar Irmansyah
+        // Mendeklarasikan Variabel Dan Melakukan Koneksi Pada Database
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        SqlConnection Conn = new SqlConnection(@"Data Source=DESKTOP-5C2L5M1\YFSERVER;Initial Catalog=DB_LATIHAN;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Membuat Fungsi untuk membersihkan Isi Dari Text Box
+        void Bersihkan()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+        }
+
+        // Membuat Fungsi untuk menampilkan data dari Database
+        void TampilBarang()
+        {
+            try
+            {
+                Conn.Open();
+                cmd = new SqlCommand("Select * from TBL_BARANG", Conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "TBL_BARANG");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "TBL_BARANG";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+
+        // Membuat fungsi untuk mencari data yang ada Data Grid View
+        void CariBarang()
+        {
+            try
+            {
+                Conn.Open();
+                cmd = new SqlCommand("Select * from TBL_BARANG where KodeBarang like '%" + textBox8.Text + "%' OR NamaBarang like '%" + textBox8.Text + "%' OR Tanggal like '%" + textBox8.Text + "%'", Conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "TBL_BARANG");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "TBL_BARANG";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+
+        // Moch Yogi Firmansyah
+        // Membuat fungsi untuk menuliskan kode secara otomatis setelah urutan terakhir
+        void KodeOtomatis()
+        {
+            long hitung;
+            string urutan;
+            SqlDataReader dr;
+            Conn.Open();
+            cmd = new SqlCommand("Select KodeBarang from TBL_BARANG where KodeBarang in(select max(KodeBarang) from TBL_BARANG) order by KodeBarang desc", Conn);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            if (dr.HasRows)
+            {
+                hitung = Convert.ToInt64(dr[0].ToString().Substring(dr["KodeBarang"].ToString().Length - 3, 3)) + 1;
+                string joinstr = "000" + hitung;
+                urutan = "BRG" + joinstr.Substring(joinstr.Length - 3, 3);
+            }
+            else
+            {
+                urutan = "BRG001";
+            }
+            dr.Close();
+            textBox1.Text = urutan;
+            Conn.Close();
+        }
+
+        // Melakukan pemanggilan fungsi Bersihkan, TampilBarang, Dan KodeOtomatis pada Form1_Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bersihkan();
+            TampilBarang();
+            KodeOtomatis();
+        }
+
+        // Melakukan input data yang akan disimpan pada Database dan ditampilkan pada Data Grid View
+        private void InputButton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum Lengkap!");
+            }
+            else
+            {
+                try
+                {
+                    Conn.Open();
+                    cmd = new SqlCommand("Insert Into TBL_BARANG values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "')", Conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Ditambahkan");
+                    Conn.Close();
+                    TampilBarang();
+                    Bersihkan();
+                    KodeOtomatis();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
+        // Apabila baris dari Data Grid View di klik, semua data yang ada dibaris tersebut akan
+        // dimunculkan pada text box
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = row.Cells["KodeBarang"].Value.ToString();
+                textBox2.Text = row.Cells["NamaBarang"].Value.ToString();
+                textBox3.Text = row.Cells["JenisBarang"].Value.ToString();
+                textBox4.Text = row.Cells["HargaBarang"].Value.ToString();
+                textBox5.Text = row.Cells["JumlahBarang"].Value.ToString();
+                textBox6.Text = row.Cells["SatuanBarang"].Value.ToString();
+            }
+            catch (Exception X)
+            {
+                MessageBox.Show(X.ToString());
+            }
+        }
+
+        // Fahmi Faqur
+        // Melakukan Ubah Data pada Database, Setelah meng-klik baris pada Data Grid View,
+        // Data yang akan diubah
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "")
+            {
+                MessageBox.Show("Data Belum Lengkap!");
+            }
+            else
+            {
+                try
+                {
+                    Conn.Open();
+                    cmd = new SqlCommand("Update TBL_BARANG set NamaBarang='" + textBox2.Text + "',JenisBarang='" + textBox3.Text + "',HargaBarang='" + textBox4.Text + "',JumlahBarang='" + textBox5.Text + "',SatuanBarang='" + textBox6.Text + "' where KodeBarang='" + textBox1.Text + "'", Conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Diubah");
+                    Conn.Close();
+                    TampilBarang();
+                    Bersihkan();
+                    KodeOtomatis();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+            }
+        }
+
+        // Melakukan Ubah Data pada Database, Setelah meng-klik baris pada Data Grid View,
+        // Data yang akan diubah
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Apakah Yakin Anda Ingin Menghapus Data Barang " + textBox2.Text + " ?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                try
+                {
+                    Conn.Open();
+                    cmd = new SqlCommand("Delete TBL_BARANG where KodeBarang='" + textBox1.Text + "'", Conn);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil DiHapus");
+                    Conn.Close();
+                    TampilBarang();
+                    Bersihkan();
+                    KodeOtomatis();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                }
+        }
+
+        // Melakukan Refresh / Membersihkan semua data yang ada di Text Box
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            TampilBarang();
+            Bersihkan();
+            KodeOtomatis();
+        }
+
+        // Memanggil fungsi CariBarang untuk mencari data yang ada di Data Grid View
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            CariBarang();
+        }
+
+        // Melakukan Print data yang ada pada Data Grid View
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            DGVPrinter printer = new DGVPrinter();
+            printer.Title = "Laporan Data Barang";
+            printer.SubTitle = string.Format("Tanggal {0}", DateTime.Now.Date.ToString("dddd-MMMM-yyyy"));
+            printer.SubTitleFormatFlags = StringFormatFlags.LineLimit | StringFormatFlags.NoClip;
+            printer.PageNumbers = true;
+            printer.PageNumberInHeader = false;
+            printer.PorportionalColumns = true;
+            printer.HeaderCellAlignment = StringAlignment.Near;
+            printer.FooterSpacing = 15;
+            printer.PrintPreviewDataGridView(dataGridView1);
+        }
+    }
+}
